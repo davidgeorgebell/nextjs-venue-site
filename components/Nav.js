2761e6775c5b5b463811276556a59f9fe845d0df
davidgeorgebell <bell.dave93@gmail.com>
2020-06-29 10:21:41 +0800

    Home page styles

---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <ul className='nav-list'>
+      <li className='nav-list-item'>Our Story</li>
+      <li className='nav-list-item'>Events</li>
+      <li className='nav-list-item'>Contact</li>
+    </ul>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Nav } from '../components/Nav';
 
 export default function Home() {
   return (
@@ -10,9 +11,27 @@ export default function Home() {
 
       <div className='content-container'>
         <main>
-          <h1 className='title'>Good Scran Cafe</h1>
+          <div className='home-grid-wrapper'>
+            <div className='home-grid-1'>
+              <span>LOGO</span>
+              <h1 className='title'>Good Scran Cafe & CO</h1>
+            </div>
+            <div className='home-grid-2'>
+              <Nav />
+              <div className='intro'>
+                <span className='decoration'></span>
+                <p className='intro-text'>
+                  Good Scran Cafe is an award winning co working cafe in the
+                  heart of Taipei, Taiwan.
+                </p>
+              </div>
+            </div>
+            <div className='home-grid-3'>
+              <img className='home-image' src='/cafe.jpg' alt='Cafe' />
+            </div>
+          </div>
         </main>
-
+        {/* 
         <footer>
           <a
             href='https://twitter.com/DavidGeorgeBell'
@@ -20,7 +39,7 @@ export default function Home() {
             rel='noopener noreferrer'>
             Made by David Bell
           </a>
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
