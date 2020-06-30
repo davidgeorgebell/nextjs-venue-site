29b6bb2b148dc0b50882dfa2406349a6e9e5a99d
davidgeorgebell <bell.dave93@gmail.com>
2020-06-30 08:59:22 +0800

    statically fetched data from airtable API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 import { Nav } from '../components/Nav';
+import { Events } from '../components/Events';
 
-export default function Home() {
+const AIRTABLE_KEY = process.env.AIRTABLE_KEY;
+
+export default function Home({ events }) {
   return (
     <div className='container'>
       <Head>
-        <title>Good Scran Cafe</title>
+        <title>The Yardies</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <header>
@@ -13,53 +16,7 @@ export default function Home() {
       </header>
       <div>
         <main>
-          <div className='hero'>
-            <div className='hero-text'>
-              <h1 className=' hero-title headline'>Good Scran Cafe</h1>
-              <p className='hero-blurb'>
-                An award winning coffee co working space that doesn't actually
-                exist and is in fact a portfolio project.
-              </p>
-            </div>
-            <div>
-              <img className='hero-image' src='/cafe.jpg' alt='cafe' />
-            </div>
-          </div>
-          <div className='intro'>
-            <h3 className='headline intro-title'>Workspace Revolution</h3>
-            <p>
-              Good Scran Cafe is an award winning coffee and Co working space.
-            </p>
-            <button className='animated-link'>Find out more</button>
-          </div>
-
-          <div className='events'>
-            <h3>Upcoming Events</h3>
-            <p>We host exciting events for the community to come together.</p>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <button className='animated-link'>See more events</button>
-          </div>
-
-          <div className='images'>
-            <h3 className='header'>Social</h3>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-            <span>IMAGE</span>
-          </div>
-
-          <div className='contact'>
-            <h3 className='header'>Contact us</h3>
-            <p>
-              You can call us if you would like some information or to hire out
-              Good Scran Cofee for an event.
-            </p>
-            <button className='animated-link'>Contact us</button>
-          </div>
+          <Events events={events} />
         </main>
 
         {/* 
@@ -74,4 +31,25 @@ export default function Home() {
       </div>
     </div>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch(
+    `https://api.airtable.com/v0/app9bv9UvKjThupFL/Events`,
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_KEY}`,
+      },
+    }
+  );
+
+  const events = await res.json();
+
+  console.log(events);
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
